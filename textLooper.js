3f0c7340d2393e677bb7ffe51bc40aa801a96050
GuMMaN <trygve@igesund.com>
2016-12-22 18:23:21 +0100

    Add files via upload

---
+++ textLooper.js
@@ -0,0 +1,25 @@
+var text = "this is the text whose letters loop fonts and colours";
+var fonts = ["Times New Roman", "lucida console", "Book Antiqua"];
+var colours = ["green", "blue", "red"];
+var textChangeSpeed = 1000; //time before the next change takes place in miliseconds
+
+
+function loopText()
+{
+    var div = $("#div"); //the place where you want your text
+    div.empty();
+    var array = new Array(text.length);
+    
+    for(i=0;i<text.length;i++)
+    {
+        var letter = $('<span></span>');
+        letter.css("font-family", fonts[Math.floor(Math.random() * fonts.length)]);
+        letter.css("color", colours[Math.floor(Math.random() * colours.length)]);
+        letter.append(text.charAt(i));
+        array[i] = letter;
+    }
+    
+    
+    div.append(array);
+    setTimeout(function() {loopText(); }, textChangeSpeed);
+}
